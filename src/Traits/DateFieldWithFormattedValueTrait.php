be6209e5713d794365a6afa5709e083d45c67e01
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-08-03 13:02:39 +0200

    Update date read formats: support for callable and custom lang formats

--- src/Traits/DateFieldWithFormattedValueTrait.php
+++ src/Traits/DateFieldWithFormattedValueTrait.php
@@ -4,9 +4,10 @@ namespace Lkt\Factory\Schemas\Traits;
 
 trait DateFieldWithFormattedValueTrait
 {
-    protected string $defaultReadFormat = '';
+    protected $defaultReadFormat = '';
+    protected array $langDefaultReadFormat = [];
 
-    public function setDefaultReadFormat(string $format): static
+    public function setDefaultReadFormat(string|callable $format): static
     {
         $this->defaultReadFormat = $format;
         return $this;
@@ -14,6 +15,24 @@ trait DateFieldWithFormattedValueTrait
 
     public function getDefaultReadFormat(): string
     {
+        if (is_callable($this->defaultReadFormat)) {
+            return call_user_func($this->defaultReadFormat);
+        }
         return $this->defaultReadFormat;
+    }
+
+    public function setLangDefaultReadFormat(string|callable $format, string $lang): static
+    {
+        $this->langDefaultReadFormat[$lang] = $format;
+        return $this;
+    }
+
+    public function getLangDefaultReadFormat(string $lang): ?string
+    {
+        if (!isset($this->langDefaultReadFormat[$lang])) return null;
+        if (is_callable($this->langDefaultReadFormat[$lang])) {
+            return call_user_func($this->langDefaultReadFormat[$lang]);
+        }
+        return $this->langDefaultReadFormat[$lang];
     }
 }
