00587cc5c0ea69db4749deecc9d31ff9485dca0e
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-08-27 19:18:37 +0200

    Be able to include an options array while reading foreign key field

--- src/Fields/ForeignKeyField.php
+++ src/Fields/ForeignKeyField.php
@@ -25,4 +25,17 @@ class ForeignKeyField extends IntegerField
     {
         return $key === $this->getName() . 'Id';
     }
+
+    protected bool $onReadIncludeOptions = false;
+
+    public function setOnReadIncludeOptions(bool $value = true): static
+    {
+        $this->onReadIncludeOptions = $value;
+        return $this;
+    }
+
+    public function hasOnReadIncludeOptions(): bool
+    {
+        return $this->onReadIncludeOptions;
+    }
 }
