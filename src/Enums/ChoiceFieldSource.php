dfa453f80f2128202c8d77e2b7e53e303d093030
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-01-06 13:32:47 +0100

    Choice fields: Support for enum definition

---
+++ src/Enums/ChoiceFieldSource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lkt\Factory\Schemas\Enums;
+
+enum ChoiceFieldSource: int
+{
+    case Array = 0;
+    case Enum = 1;
+}
--- src/Traits/FieldWithChoiceOptionTrait.php
+++ src/Traits/FieldWithChoiceOptionTrait.php
@@ -2,6 +2,9 @@
 
 namespace Lkt\Factory\Schemas\Traits;
 
+use Lkt\Factory\Schemas\Enums\ChoiceFieldSource;
+use function Lkt\Tools\Enums\enumToArray;
+
 trait FieldWithChoiceOptionTrait
 {
     protected array $allowedOptions = [];
@@ -11,11 +14,25 @@ trait FieldWithChoiceOptionTrait
     protected bool $enabledEmptyPreset = false;
     protected string|int|null $emptyDefault = null;
     protected string $i18nViewOptions = '';
+    protected string $enumChoiceClass = '';
+
+    protected ChoiceFieldSource $optionsSource = ChoiceFieldSource::Array;
 
     final public function setAllowedOptions(array $options): static
     {
         $this->allowedOptions = $options;
         return $this;
+    }
+
+    final public function setEnumChoiceClass(string $enumChoiceClass): static
+    {
+        $this->enumChoiceClass = $enumChoiceClass;
+        return $this;
+    }
+
+    final public function getEnumChoiceClass(): string
+    {
+        return $this->enumChoiceClass;
     }
 
     final public function getAllowedOptions(): array
@@ -26,6 +43,15 @@ trait FieldWithChoiceOptionTrait
     final public static function choice(array $options, string $name, string $column = ''): static
     {
         return (new static($name, $column))->setAllowedOptions($options);
+    }
+
+    final public static function enumChoice(string $enumChoiceClass, string $name, string $column = ''): static
+    {
+        $r = (new static($name, $column))
+            ->setEnumChoiceClass($enumChoiceClass)
+            ->setAllowedOptions(enumToArray($enumChoiceClass));
+        $r->optionsSource = ChoiceFieldSource::Enum;
+        return $r;
     }
 
     final public function addComparatorIn(string $name, array $values): static
